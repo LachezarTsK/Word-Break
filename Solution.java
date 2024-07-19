f65dc1acbcec0758b5f5a4554406379cd93fa992
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-20 00:25:24 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,119 +1,83 @@
+
 import java.util.List;
 import java.util.LinkedList;
-import java.util.Set;
-import java.util.HashSet;
 
 public class Solution {
 
-    /*
-    Key points to observe:
-    
-    1. A greedy search in the Trie will not do the job.
-       There might be some tricky overlaps, so all possible combinations must be checked.
-    
-       Example: 
-       s = "codexzyone";  wordDict = ["code", "one", "codexzy"].
-    
-       In the above case, a greedy approach will finish the search after the word "code" 
-       and will return "false". But, in fact, the correct answer is "true".
-        
-    2. On the other hand, trying all possible combinations might lead to repeating the same search, 
-        so the algorithm should avoid the other extreme, namely: starting the search from one and the same 
-        index, multiple times.
-    
-        Example:        
-        s = "xxy";  wordDict = ["x", "xx"]. 
-    
-        Without such safeguards, in the above case, the algorithm will start the search from index 2, 
-        the letter "y", twice. With large input, the time complexity will be prohibitive 
-        and at some point will exceed the time limits.
-     */
-    
-    LinkedList<Integer> queue_startIndexes;// start indexes for the substring to be searched.
-    Set<Integer> visited_startIndexes;
-    int inputStringLength;
-    int lastIndexOfCurrentWord;
-    TrieNode root;
+    private LinkedList<Integer> queueStartIndexes;
+    private boolean[] visitedStartIndexes;
+    private final Trie trie = new Trie();
 
-    public boolean wordBreak(String s, List<String> wordDict) {
-        inputStringLength = s.length();
-        root = new TrieNode();
+    public boolean wordBreak(String input, List<String> wordDict) {
+        visitedStartIndexes = new boolean[input.length() + 1];
         addAllWordsToTrie(wordDict);
-        return stringCanBeSegmented_intoDictionaryWords(s);
-    }
-
-    public boolean stringCanBeSegmented_intoDictionaryWords(String s) {
-
-        queue_startIndexes = new LinkedList<>();
-        queue_startIndexes.add(0);
-
-        visited_startIndexes = new HashSet<>();
-        visited_startIndexes.add(0);
-
-        while (!queue_startIndexes.isEmpty()) {
-            int current = queue_startIndexes.removeFirst();
-            isWord_inDictionary(s.substring(current));
-            if (lastIndexOfCurrentWord == s.length()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void isWord_inDictionary(String word) {
-
-        TrieNode current = root;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-
-            if (current.branches[ch - 'a'] == null) {
-                break;
-            }
-
-            current = current.branches[ch - 'a'];
-            if (current.isEndOfWord && visited_startIndexes.add(inputStringLength - word.length() + i + 1)) {
-
-                queue_startIndexes.add(inputStringLength - word.length() + i + 1);
-                lastIndexOfCurrentWord = inputStringLength - word.length() + i + 1;
-
-                if (lastIndexOfCurrentWord == inputStringLength) {
-                    break;
-                }
-            }
-        }
+        return stringCanBeSegmentedIntoDictionaryWords(input);
     }
 
     public void addAllWordsToTrie(List<String> wordDict) {
         for (int i = 0; i < wordDict.size(); i++) {
-            addWord(wordDict.get(i));
+            trie.addWord(wordDict.get(i));
         }
     }
 
-    public void addWord(String word) {
+    public boolean stringCanBeSegmentedIntoDictionaryWords(String input) {
+        queueStartIndexes = new LinkedList<>();
+        queueStartIndexes.add(0);
+        visitedStartIndexes[0] = true;
 
+        while (!queueStartIndexes.isEmpty()) {
+            if (queueStartIndexes.getLast() == input.length()) {
+                return true;
+            }
+            int startIndex = queueStartIndexes.removeFirst();
+            trie.addStartIndexesSubstringsToQueue(input.substring(startIndex), input.length(), queueStartIndexes, visitedStartIndexes);
+        }
+        return false;
+    }
+}
+
+class Trie {
+
+    private class TrieNode {
+
+        private static final int ALPHABET_SIZE = 26;
+        TrieNode[] branches = new TrieNode[ALPHABET_SIZE];
+        boolean isEndOfWord;
+    }
+
+    private final TrieNode root = new TrieNode();
+
+    public void addWord(String word) {
         TrieNode current = root;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-
             if (current.branches[ch - 'a'] == null) {
                 current.branches[ch - 'a'] = new TrieNode();
             }
             current = current.branches[ch - 'a'];
-            if (i == word.length() - 1) {
-                current.isEndOfWord = true;
-            }
         }
+        current.isEndOfWord = true;
     }
 
-}
+    public void addStartIndexesSubstringsToQueue(String word, int inputLength, LinkedList<Integer> queueStartIndexes, boolean[] visitedStartIndexes) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
 
-class TrieNode {
+            int index = word.charAt(i) - 'a';
+            if (current.branches[index] == null) {
+                break;
+            }
 
-    boolean isEndOfWord;
-    TrieNode[] branches;
-    final int CHARS_IN_ALPHABET = 26;
+            current = current.branches[index];
+            if (!current.isEndOfWord || visitedStartIndexes[inputLength - word.length() + i + 1]) {
+                continue;
+            }
 
-    public TrieNode() {
-        branches = new TrieNode[CHARS_IN_ALPHABET];
+            queueStartIndexes.add(inputLength - word.length() + i + 1);
+            visitedStartIndexes[inputLength - word.length() + i + 1] = true;
+            if (queueStartIndexes.peekLast() == inputLength) {
+                break;
+            }
+        }
     }
 }
